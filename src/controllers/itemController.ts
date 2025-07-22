eca7bb1cd51b773ea00071bb0c84f5e8f2df1201
thisisaldi <m.naufaldi.aldi@gmail.com>
2025-07-22 21:08:41 +0700

    feat(talent-growth): Add CRUD for Item

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -22,7 +22,49 @@ export const getItems = async (req: Request, res: Response) => {
     const items = await Item.find();
     res.json(items);
   } catch (err) {
-    console.log("Get Items : " + err);
+    console.log("Get Items Error : " + err);
+    res.status(500).send("Server error");
+  }
+};
+
+export const getItemById = async (req: Request, res: Response) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).json({ msg: "Item not found" });
+    res.json(item);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+};
+
+export const updateItem = async (req: Request, res: Response) => {
+  const { name, description } = req.body;
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).json({ msg: "Item not found" });
+
+    item.name = name || item.name;
+    item.description = description || item.description;
+
+    await item.save();
+    res.json(item);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+};
+
+export const deleteItem = async (req: Request, res: Response) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ msg: "Item not found" });
+    }
+
+    await Item.findByIdAndDelete(req.params.id); 
+    res.json({ msg: "Item deleted successfully" });
+  } catch (err: unknown) {
+    const error = err as Error;
+    console.error("Delete Item Error:", error.message);
     res.status(500).send("Server error");
   }
 };
--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -1,8 +1,11 @@
-import { createItem, getItems } from "../controllers/itemController";
+import { createItem, deleteItem, getItemById, getItems, updateItem } from "../controllers/itemController";
 import express from "express";
 
 export const itemRouter = express.Router();
 
 itemRouter.post("/", createItem);
 itemRouter.get("/", getItems);
+itemRouter.get("/:id", getItemById);
+itemRouter.put("/:id", updateItem);
+itemRouter.delete("/:id", deleteItem);
 
