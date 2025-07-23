2aa029fdd10124ee4b20860071136138a0c139b4
thisisaldi <m.naufaldi.aldi@gmail.com>
2025-07-23 12:39:03 +0700

    feat(talent-growth): Add owned Item to user profile

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,22 @@
 import { Response } from "express";
 import { User } from "../models/Users";
 import { AuthRequest } from "../types/authRequest";
+import { Item } from "../models/Items";
 
 export const getUserProfile = async (req: AuthRequest, res: Response) => {
   try {
     const user = await User.findById(req.user?.id).select("_id name email");
 
     if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.json(user);
+    const items = await Item.find({ owner: user._id }).select(
+      "_id name description"
+    );
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      items,
+    });
   } catch (err) {
     res.status(500).send({ message: "Server error" });
   }
